6862e5a1f48fb260d63071bdb14209ca74a91ff5
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-10-02 20:31:33 +0300

    complete oxford lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,19 @@
 def oxford_comma(items):
-    return None
+    # items is a list of strings
+    if len(items) == 2:
+        str = ' and '.join(items)
+        return str
+    
+    elif len(items) > 2:
+        # If the length of the items of the list is more than 2
+        # Join all the words before that str with ","
+        # join the last word with that new str with "and" 
+        str1 = ', '.join(items[:-1])
+        print(f"{str1}, and {items[-1]}")
+        return f"{str1}, and {items[-1]}"
+    
+    else:
+        str = ''.join(items)
+        return str
+
+oxford_comma(["kiwi", "durian", "starfruit"])
